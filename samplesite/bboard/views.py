2fdf1aa16a87e2a92ded59ae43bcb71d7d1fefbc
Nastenko Peter <source-2306@mail.ru>
2022-08-26 14:58:04 +0300

    вывод текста из базы

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -2,7 +2,11 @@
 # определенного формата и в ответ выводящий на экран определенную веб-страницу.(viev - устаревшее)
 
 from django.http import HttpResponse
+from .models import Bd
 
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    s = 'Список объявлений\n\n\n'
+    for bb in Bd.objects.order_by('-published'):
+        s += bb.title + '\n' + bb.content + '\n\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
